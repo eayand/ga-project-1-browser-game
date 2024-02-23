d66913dc913f2fe5b12afff45f1b5ac66be13e83
LY <eayetc@gmail.com>
2024-02-23 14:57:37 -0800

    added sections from instructions

--- js/main.js
+++ js/main.js
@@ -11,3 +11,18 @@ const buttons = defaultButtons.sort(() => Math.random() - 0.5)
 console.log(buttons)
 
 //create a button for each item in the array with its hex code as its ID or some feature and then use the ID of the clicked button to fill the background
+
+
+  /*----- constants -----*/
+
+
+  /*----- state variables -----*/
+
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
