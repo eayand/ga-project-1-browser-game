4a6407cc8fdeb317804a824514a789e00263422b
LY <eayetc@gmail.com>
2024-02-24 22:18:12 -0800

    started javascript

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
-
-
-// freeCodeCamp 
-// const myArray = ["apple", "banana", "cherry", "date", "elderberry"]; 
-// const shuffle = myArray.sort(() => Math.random() - 0.5); 
-// console.log(shuffle);
-
-const defaultButtons = ['black', 'blue', 'green', 'red', 'white', 'yellow']
-//change to hex
-const buttons = defaultButtons.sort(() => Math.random() - 0.5)
-console.log(buttons)
-
-//create a button for each item in the array with its hex code as its ID or some feature and then use the ID of the clicked button to fill the background
-
-
   /*----- constants -----*/
+const colors = ['#ff3333', '#ffa500', '#fff000', '#00bb60', '#4444ff', '#9900dd']
+
+let secretCode = []
+let buttons = []
 
 
   /*----- state variables -----*/
 
 
   /*----- cached elements  -----*/
-
+const choiceButtons = document.querySelectorAll('.choice')
 
   /*----- event listeners -----*/
 
 
   /*----- functions -----*/
+init()
+
+function init() {
+    shuffle()
+}
+
+function shuffle() {
+    const black = '#000000'
+    buttons = colors.sort(() => Math.random() - 0.5)
+    choiceButtons.forEach(function(choiceButton, idx) {
+      const pickedColor = buttons[idx]
+      choiceButton.style.backgroundColor = pickedColor
+    })
+}
+
